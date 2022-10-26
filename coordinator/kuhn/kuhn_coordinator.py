e0fa959bcdb6fbf59b74573b279830656175844c
Bagaev Dmitry <bvdmitri@gmail.com>
2022-10-26 09:28:20 +0200

    Update kuhn_coordinator.py

--- coordinator/kuhn/kuhn_coordinator.py
+++ coordinator/kuhn/kuhn_coordinator.py
@@ -292,7 +292,7 @@ class KuhnCoordinator(object):
     def play_tournament(self, players: List[Player]):
 
         # Indicate that the tournament has started
-        Tournament.objects.get(coordinator__id = self.id).update(is_started = True)
+        Tournament.objects.filter(coordinator__id = self.id).update(is_started = True)
 
         # First we create tournament bracket based on number of players
         dbtournament = Tournament.objects.get(coordinator__id = self.id)
